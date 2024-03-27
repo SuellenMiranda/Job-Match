009ce4145f62f02c3c28cac784d9e853257816f6
MontebellerZ <vyatrhjar@gmail.com>
2024-03-27 09:01:01 -0300

    feat: formulário de cadastro de usuários

--- src/bases/initial/index.tsx
+++ src/bases/initial/index.tsx
@@ -1,18 +1,15 @@
 import { Image, View } from "react-native";
 import styles from "./styles";
 import { Outlet } from "react-router-native";
-import SplashScreen from "../../../assets/splash_gradient.png";
 import Logo from "../../../assets/icon.png";
 import { LinearGradient } from "expo-linear-gradient";
 
 function Initial() {
     return (
         <View style={styles.container}>
-            {/* <Image source={SplashScreen} style={styles.imageBackground} /> */}
-
             <LinearGradient
                 colors={["#ffffff", "#f2685d", "#ef4074", "#ee3b76"]}
-                locations={[0.1, 0.7, 0.9, 1]}
+                locations={[0.2, 0.6, 0.9, 1]}
                 style={styles.gradientBackground}
             />
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -11,13 +11,20 @@ import { useState } from "react";
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 function Login() {
-    const [login, setLogin] = useState("");
-    const [pass, setPass] = useState("");
-    const [email, setEmail] = useState("");
+    const [loginUser, setLoginUser] = useState("");
+    const [loginPass, setLoginPass] = useState("");
+    const [resetEmail, setResetEmail] = useState("");
+    const [newEmail, setNewEmail] = useState("");
+    const [newUser, setNewUser] = useState("");
+    const [newPass, setNewPass] = useState("");
 
+    const boxPosition0 = useSharedValue(-SCREEN_WIDTH);
     const boxPosition1 = useSharedValue(0);
     const boxPosition2 = useSharedValue(SCREEN_WIDTH);
 
+    const boxAnimated0 = useAnimatedStyle(() => ({
+        transform: [{ translateX: withTiming(boxPosition0.value) }],
+    }));
     const boxAnimated1 = useAnimatedStyle(() => ({
         transform: [{ translateX: withTiming(boxPosition1.value) }],
     }));
@@ -26,37 +33,76 @@ function Login() {
     }));
 
     const slideForm = (index: number) => {
-        boxPosition1.value = index === 0 ? 0 : -SCREEN_WIDTH;
-        boxPosition2.value = index === 1 ? 0 : SCREEN_WIDTH;
+        boxPosition0.value = index === 0 ? 0 : -SCREEN_WIDTH;
+        boxPosition1.value = index === 0 ? SCREEN_WIDTH : index === 2 ? -SCREEN_WIDTH : 0;
+        boxPosition2.value = index === 2 ? 0 : SCREEN_WIDTH;
     };
 
     return (
         <View style={styles.container}>
             <View style={styles.boxGroup}>
-                <Animated.View style={[styles.boxForm, boxAnimated1]}>
-                    <Text style={styles.boxTitle}>Entrar</Text>
+                <Animated.View style={[styles.boxForm, boxAnimated0]}>
+                    <Text style={styles.boxTitle}>Cadastro</Text>
 
                     <TextInput
                         style={styles.inputForm}
-                        value={login}
-                        onChangeText={setLogin}
-                        placeholder="Username / Email"
+                        value={newUser}
+                        onChangeText={setNewUser}
+                        placeholder="Username"
                     />
 
                     <TextInput
                         style={styles.inputForm}
-                        value={pass}
-                        onChangeText={setPass}
+                        value={newEmail}
+                        onChangeText={setNewEmail}
+                        placeholder="Email"
+                    />
+
+                    <TextInput
+                        style={styles.inputForm}
+                        value={newPass}
+                        onChangeText={setNewPass}
                         placeholder="Senha"
                         secureTextEntry
                     />
 
                     <TouchableOpacity style={styles.submitBtn} onPress={() => slideForm(0)}>
-                        <Text style={styles.submitBtnText}>Entrar</Text>
+                        <Text style={styles.submitBtnText}>Cadastrar-se</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={styles.navigateBtn} onPress={() => slideForm(1)}>
+                        <Text style={styles.navigateBtnText}>Voltar para o Login!</Text>
+                    </TouchableOpacity>
+                </Animated.View>
+
+                <Animated.View style={[styles.boxForm, boxAnimated1]}>
+                    <Text style={styles.boxTitle}>Entrar</Text>
+
+                    <TextInput
+                        style={styles.inputForm}
+                        value={loginUser}
+                        onChangeText={setLoginUser}
+                        placeholder="Username / Email"
+                    />
+
+                    <TextInput
+                        style={styles.inputForm}
+                        value={loginPass}
+                        onChangeText={setLoginPass}
+                        placeholder="Senha"
+                        secureTextEntry
+                    />
+
+                    <TouchableOpacity style={styles.submitBtn} onPress={() => slideForm(1)}>
+                        <Text style={styles.submitBtnText}>Entrar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.navigateBtn} onPress={() => slideForm(2)}>
                         <Text style={styles.navigateBtnText}>Esqueci minha senha</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.navigateBtn} onPress={() => slideForm(0)}>
+                        <Text style={styles.navigateBtnText}>Não tem conta? Cadastre-se!</Text>
                     </TouchableOpacity>
                 </Animated.View>
 
@@ -65,19 +111,17 @@ function Login() {
 
                     <TextInput
                         style={styles.inputForm}
-                        value={email}
-                        onChangeText={setEmail}
+                        value={resetEmail}
+                        onChangeText={setResetEmail}
                         placeholder="Email"
                     />
 
-                    <TouchableOpacity style={styles.submitBtn} onPress={() => slideForm(0)}>
+                    <TouchableOpacity style={styles.submitBtn} onPress={() => slideForm(2)}>
                         <Text style={styles.submitBtnText}>Gerar nova senha</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.navigateBtn} onPress={() => slideForm(0)}>
-                        <Text style={styles.navigateBtnText}>
-                            Já posssui conta? Faça seu login!
-                        </Text>
+                    <TouchableOpacity style={styles.navigateBtn} onPress={() => slideForm(1)}>
+                        <Text style={styles.navigateBtnText}>Voltar para o login!</Text>
                     </TouchableOpacity>
                 </Animated.View>
             </View>
